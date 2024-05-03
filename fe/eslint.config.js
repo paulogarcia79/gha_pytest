539bcbd2a88c8f21f5a53ba70051843dcd754729
paulo_garcia <my.name@mapanacode.com>
2024-05-03 15:01:57 -0500

    [main] chore: Update eslint.config.js with recommended configurations for Vue and browser globals

---
+++ fe/eslint.config.js
@@ -0,0 +1,10 @@
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+];
