a8aa00f27724228f5e4505da0aee0d906ee96e0b
paulo_garcia <my.name@mapanacode.com>
2024-05-08 15:57:33 -0500

    [main] Import "globals" module in eslint configuration

--- fe/eslint.config.js
+++ fe/eslint.config.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import eslintPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierVue from 'eslint-plugin-prettier-vue';
 import eslintPluginVue from "eslint-plugin-vue";
+import globals from "globals";
 
 
 // export default [
