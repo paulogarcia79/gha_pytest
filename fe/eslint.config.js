8a34c05169d78afec4ebea1c8839904c5ede961a
paulo_garcia <my.name@mapanacode.com>
2024-05-08 17:36:33 -0500

    [main] chore: Update eslint.config.js to include eslintPrettier plugin

--- fe/eslint.config.js
+++ fe/eslint.config.js
@@ -1,20 +1,35 @@
 import eslint from '@eslint/js';
 import eslintPrettier from 'eslint-config-prettier';
-import eslintPluginPrettierVue from 'eslint-plugin-prettier-vue/lib/index.js';
+import eslintPluginPrettierVue from 'eslint-plugin-prettier-vue';
 import eslintPluginVue from "eslint-plugin-vue";
 import globals from "globals";
 
 
-// export default [
-//   {languageOptions: { globals: globals.browser }},
-//   pluginJs.configs.recommended,
-//   ...pluginVue.configs["flat/essential"],
-// ];
+/**
+ * Eslint configuration options.
+ * @typedef {Object} EslintConfig
+ * @property {Object} languageOptions - Language options for eslint.
+ * @property {Object} eslintConfig - Recommended eslint configuration.
+ * @property {Object} eslintPrettier - Eslint prettier configuration.
+ * @property {Object} plugins - Eslint plugins.
+ * @property {Object} eslintPluginVue - Eslint plugin for Vue.
+ * @property {Object} eslintPluginPrettierVue - Eslint plugin for prettier Vue.
+ */
 
+/**
+ * Eslint configuration array.
+ * @type {EslintConfig[]}
+ */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+  },
   eslint.configs.recommended,
-  ...eslintPluginVue.configs["flat/essential"],
-  eslintPluginPrettierVue,
   eslintPrettier,
-]
+  {
+    plugins: {
+      eslintPluginVue: eslintPluginVue,
+      eslintPluginPrettierVue: eslintPluginPrettierVue,
+    },
+  },
+];
