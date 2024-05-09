71976b3c338cbaebe71dd427325087a2b25ee74f
paulo_garcia <my.name@mapanacode.com>
2024-05-09 11:30:07 -0500

    [main] chore: Update e2e test to log the baseURL

--- fe/e2e/test.spec.js
+++ fe/e2e/test.spec.js
@@ -2,6 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('should display welcome message', async ({ page }) => {
     await page.goto('/');
+    console.log('baseURL:', page.url());
     const el = await page.locator('h1');
     await expect (el).toHaveText('Hello, Vue 3!');
   });
