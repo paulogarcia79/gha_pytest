9860f47aeccc449b9e479b5d63696495e9562131
paulo_garcia <my.name@mapanacode.com>
2024-05-09 11:03:58 -0500

    [main] chore: Update page URL in e2e test

--- fe/e2e/test.spec.js
+++ fe/e2e/test.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('should display welcome message', async ({ page }) => {
-    await page.goto('http://localhost:5173');
+    await page.goto('/');
     const el = await page.locator('h1');
     await expect (el).toHaveText('Hello, Vue 3!');
   });
